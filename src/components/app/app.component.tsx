7dfbae7c00010b272a509ad2b478b233e97625cc
MariusMonkam <cloudmonde@yahoo.com>
2023-01-08 18:18:56 +0200

    styling with meterial ui & integrating react-hook-form

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -9,18 +9,38 @@ import Home from "../../pages/home/home.page";
 import "../../common/styles";
 import "./app.component.css";
 import client from "../../common/apollo-client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
+import { Paper } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/about" element={<About />} />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Paper>
+            <Header />
+          </Paper>
 
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+          <Routes>
+            <Route path="/about" element={<About />} />
+
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </ApolloProvider>
   );
 };
--- src/components/posts/posts-grid/posts-grid-item/posts-grid-item.component.tsx
+++ src/components/posts/posts-grid/posts-grid-item/posts-grid-item.component.tsx
@@ -1,14 +1,38 @@
+import {
+  Avatar,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 import { IPost } from "../../../../common/interfaces/post.interface";
 import "./posts-grid-item.component.css";
 
 const PostGridItem: React.FC<{ post: IPost }> = ({ post }: { post: IPost }) => {
+  const theme = useTheme();
   return (
-    <div>
-      <p>{post.id}</p>
-      <p>{post.title}</p>
-      <p>{post.body}</p>
-    </div>
+    <ListItem
+      sx={{
+        padding: 1,
+        margin: 1,
+        maxWidth: "100%",
+      }}
+    >
+      <ListItemAvatar>
+        <Avatar sx={{ bgcolor: theme.palette.primary.main }}>{post.id}</Avatar>
+      </ListItemAvatar>
+
+      <ListItemText
+        primaryTypographyProps={{
+          fontSize: 20,
+          fontWeight: "bold",
+          letterSpacing: 0,
+        }}
+        primary={post.title}
+        secondary={post.body}
+      />
+    </ListItem>
   );
 };
 
