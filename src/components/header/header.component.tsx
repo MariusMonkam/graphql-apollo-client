bdd62290353cf0fa3196bc51255ef342c893fc39
MariusMonkam <cloudmonde@yahoo.com>
2023-01-15 20:16:20 +0200

    responsive navbar

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,29 +1,136 @@
 //import { useTheme } from "@mui/material/styles";
-
-import { Toolbar } from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import React from "react";
+import React, { ReactElement } from "react";
 import { NavLink } from "react-router-dom";
+import {
+  Box,
+  Link,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+import MenuIcon from "@mui/icons-material/Menu";
+import { routes } from "../routes/routes";
 
 import "./header.component.css";
 
-const Header: React.FC = () => {
-  return (
-    <header>
-      <AppBar component="nav">
-        <Toolbar>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
+const Header: React.FC = (): ReactElement => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
 
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-          </ul>
+  const handleOpenNavMenu = (e: any) => {
+    setAnchorElNav(e.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "auto",
+        backgroundColor: "primary.main",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            color="secondary.main"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+            }}
+          >
+            Graphql App
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {routes.map((page) => (
+                <Link
+                  key={page.key}
+                  component={NavLink}
+                  to={page.path}
+                  color="secondary.main"
+                  underline="none"
+                  variant="button"
+                >
+                  <MenuItem onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page.title}</Typography>
+                  </MenuItem>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            color="secondary.main"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            Graphql App
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                marginLeft: "1rem",
+              }}
+            >
+              {routes.map((page) => (
+                <Link
+                  key={page.key}
+                  component={NavLink}
+                  to={page.path}
+                  underline="none"
+                  color="white"
+                  variant="button"
+                  sx={{ fontSize: "large", marginLeft: "2rem" }}
+                >
+                  {page.title}
+                </Link>
+              ))}
+            </Box>
+          </Box>
         </Toolbar>
-      </AppBar>
-    </header>
+      </Container>
+    </Box>
   );
 };
 
