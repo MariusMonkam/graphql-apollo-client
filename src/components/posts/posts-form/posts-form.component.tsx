b498680b279873990c33a022dd0fd8231a9c2894
MariusMonkam <cloudmonde@yahoo.com>
2023-01-16 11:47:49 +0200

    fixing ui form

--- src/components/posts/posts-form/posts-form.component.tsx
+++ src/components/posts/posts-form/posts-form.component.tsx
@@ -3,12 +3,10 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useForm, Controller } from "react-hook-form";
 import { useCreatePost } from "../../../hooks/posts/useCreatePost";
-import { TextField } from "@mui/material";
+import { TextField, Card, CardContent, Grid } from "@mui/material";
 
 import "./posts-form.component.css";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Paper from "@mui/material/Paper";
 import { useTheme } from "@mui/material/styles";
 
 interface FormData {
@@ -39,65 +37,68 @@ const PostsForm: React.FC = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <Paper elevation={3}>
-        <Box
-          sx={{
-            width: "100%",
-            height: "auto",
-          }}
-        >
-          <Controller
-            name="title"
-            control={control}
-            defaultValue="Title of my post"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Post title"
-                variant="outlined"
-                fullWidth
-                error={!!errors.title}
-                helperText={errors.title ? errors.title?.message : ""}
+      <Card>
+        <CardContent>
+          <Grid container spacing={1}>
+            <Grid xs={12} item>
+              <Controller
+                name="title"
+                control={control}
+                defaultValue="Title of my post"
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="Post title"
+                    variant="outlined"
+                    fullWidth
+                    error={!!errors.title}
+                    helperText={errors.title ? errors.title?.message : ""}
+                  />
+                )}
               />
-            )}
-          />
+            </Grid>
+            <Grid xs={12} item>
+              <Controller
+                name="body"
+                control={control}
+                defaultValue="Body of my post"
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    label="Post body"
+                    multiline
+                    rows={7}
+                    variant="outlined"
+                    fullWidth
+                    error={!!errors.body}
+                    helperText={errors.body ? errors.body?.message : ""}
+                  />
+                )}
+              />
+            </Grid>
 
-          <Controller
-            name="body"
-            control={control}
-            defaultValue="Body of my post"
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Post body"
-                multiline
-                rows={7}
-                variant="outlined"
-                fullWidth
-                error={!!errors.body}
-                helperText={errors.body ? errors.body?.message : ""}
-              />
-            )}
-          />
-        </Box>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingBottom: 10,
-            marginBottom: 10,
-          }}
-        >
-          <Button
-            variant="contained"
-            sx={{ backgroundColor: theme.palette.primary.main }}
-          >
-            Submit
-            <input hidden type="submit" />
-          </Button>
-        </div>
-      </Paper>
+            <Grid xs={12} item>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  paddingBottom: 10,
+                  marginBottom: 10,
+                }}
+              >
+                <Button
+                  variant="contained"
+                  sx={{ backgroundColor: theme.palette.primary.main }}
+                >
+                  Submit
+                  <input hidden type="submit" />
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </form>
   );
 };
